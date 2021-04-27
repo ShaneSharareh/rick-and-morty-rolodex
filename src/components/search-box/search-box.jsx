6565e504baefddae51b96dbf600333023b6be924
shanesharareh <shane.s.sharareh@gmail.com>
2021-04-27 16:26:03 -0700

    fix offset bug

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 import './search-box.css';
 
 export const SearchBox = ({placeholder, handleSearchChange}) => (
-    <div className="wrap">    <input className='search' type='search' placeholder = {placeholder} onChange={handleSearchChange}/>
-    <button className='searchButton'><i className="fa fa-search"></i></button>
-    </div>
+       <input className='search' type='search' placeholder = {placeholder} onChange={handleSearchChange}/>
+   
 
 )
