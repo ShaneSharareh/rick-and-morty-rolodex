a8e263745984c68ff6cbdd4790751740731b235b
shanesharareh <shane.s.sharareh@gmail.com>
2021-04-27 15:45:11 -0700

    fix background and searchbar

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
-import './search-box';
+import './search-box.css';
 
 export const SearchBox = ({placeholder, handleSearchChange}) => (
-    <input className='search' type='search' placeholder = {placeholder} onChange={handleSearchChange}/>
+    <div className="wrap">    <input className='search' type='search' placeholder = {placeholder} onChange={handleSearchChange}/>
+    <button className='searchButton'><i className="fa fa-search"></i></button>
+    </div>
+
 )
